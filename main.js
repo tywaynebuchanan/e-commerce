ce47c9b176ba50950d3c3a326ebbb4685105585b
Tywayne <tywaynebuchanan@gmail.com>
2021-02-24 17:23:21 -0500

    chore: made changes to website and made it mobile

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 function myFunction() {
     alert('This works');
 }
+
+const navMenu = document.getElementById('menu-nav');
+const closeBtn = document.getElementById('close-btn');
+document.getElementById('menu-btn').addEventListener('click',function(){
+    navMenu.style.display = 'block';
+    closeBtn.style.display = 'block';
+})
+
+document.getElementById('close-btn').addEventListener('click',function(){
+    navMenu.style.display ='none';
+})
